80fc67858771c2df7a6d3ab4863114e86c8d9b1b
Madi-S <khovansky99@gmail.com>
2021-08-17 14:28:50 +0600

    Pagination implemented

--- src/components/People.js
+++ src/components/People.js
@@ -1,20 +1,39 @@
+import { useState } from 'react'
 import { useQuery } from 'react-query'
 import { sleep } from './../utils'
 import Human from './Human'
 import Loader from 'react-loader-spinner'
 
-const fetchPeople = async () => {
+const fetchPeople = async key => {
+    const [endpoint, page] = key.queryKey
     await sleep(2500)
-    const res = await fetch('http://swapi.dev/api/people')
+    const res = await fetch(`http://swapi.dev/api/people?page=${page}`)
     return res.json()
 }
 
 const People = () => {
-    const { data, isLoading, error } = useQuery('people', fetchPeople)
+    const [page, setPage] = useState(1)
+    const { data, isLoading, error } = useQuery(['people', page], fetchPeople)
 
     return (
         <div>
-            <h2>People</h2>
+            <h2>People #{page}</h2>
+            <div className='pagination'>
+                <button
+                    onClick={() => {
+                        if (page > 1) setPage(page - 1)
+                    }}
+                >
+                    Back
+                </button>
+                <button
+                    onClick={() => {
+                        if (page < 6) setPage(page + 1)
+                    }}
+                >
+                    Forward
+                </button>
+            </div>
 
             {error && <div className='error'>Error fetching data</div>}
             {isLoading && (
